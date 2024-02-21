06d33c61438bf2eb0ca9fb2138368e2f9f420850
Amr Al Iraqi <148067288+amraliraqi@users.noreply.github.com>
2024-02-21 15:53:55 +0200

    Add files via upload

--- Assignment 3/Assignment 3 Q5.v
+++ Assignment 3/Assignment 3 Q5.v
@@ -1,35 +1,25 @@
-module SLE #(parameter LAT=0, ADn=0, SD=1)(
+module SLE (
 	input D,CLK,EN,
-	input ALn,
-	input SLn,
+	input ALn, ADn,
+	input SLn, SD,
+  input LAT,
 	output Q
 	);
  
-      reg Q_reg, Q_next ;
-      generate
-        if(~LAT) begin
+          reg Q_reg_ff,Q_reg_latch; 
+          reg Q_next ;
+
           always @(posedge CLK or negedge ALn)
             if(~ALn)
-              Q_reg <= ~ADn;
+              Q_reg_ff <= ~ADn;
             else 
-              Q_reg <= Q_next;
+              Q_reg_ff <= Q_next;
 
-          always @(*)
-            if(EN)
-              if(~SLn)
-                Q_next=SD;
-             else 
-                Q_next=D;
-            else 
-              Q_next=Q_reg;
-          assign Q=Q_reg;    
-        end       
-        else begin
           always @(*)
             if(~ALn)
-              Q_reg<=~ADn;
+              Q_reg_latch<=~ADn;
             else if(CLK)
-              Q_reg<=Q_next;
+              Q_reg_latch<=Q_next;
 
           always @(*)
             if(EN)
@@ -37,11 +27,10 @@ module SLE #(parameter LAT=0, ADn=0, SD=1)(
                 Q_next=SD;
              else 
                 Q_next=D;
-            else 
-              Q_next=Q_reg;
-          assign Q=Q_reg;     
-        end                          
-      endgenerate
+            else    
+             Q_next= (LAT) ? Q_reg_latch:Q_reg_ff;
+
+          assign Q= (LAT) ? Q_reg_latch:Q_reg_ff;     
 
 endmodule      
 
@@ -49,17 +38,35 @@ endmodule
 module SLE_tb();
 
   reg D,CLK,EN;
-  reg ALn;
-  reg SLn;
+  reg ALn, ADn;
+  reg SLn, SD;
+  reg LAT;
   wire Q;
-  SLE  #(0,0,1)dut (D, CLK, EN, ALn, SLn,Q) ;
+  SLE dut (D, CLK, EN, ALn, ADn, SLn, SD, LAT, Q) ;
   
   initial begin
     CLK=0;
     forever
     #10 CLK=~CLK;
   end
+
   initial begin
+    D=0;
+    ALn=0;
+    ADn=0;
+    EN=0;
+    SLn=0;
+    SD=1;
+    LAT=0;
+    repeat(2) @(negedge CLK);
+    ALn=1;
+    @(negedge CLK);
+    EN=1;
+    @(negedge CLK);
+    SLn=1;
+    repeat(2) @(negedge CLK);
+    LAT=1;
+    @(negedge CLK);
     D=0;
     ALn=0;
     EN=0;
@@ -71,6 +78,7 @@ module SLE_tb();
     @(negedge CLK);
     SLn=1;
     repeat(2) @(negedge CLK);
+
     $stop;
   end
 endmodule
